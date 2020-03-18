7e1bf5f1f287f5e31488b95f4ca5266ec011311f
Juan BC <jbc.develop@gmail.com>
2020-03-18 10:40:57 -0300

    load is explicit

--- data/__init__.py
+++ data/__init__.py
@@ -3,13 +3,23 @@ import pathlib
 
 import joblib
 
+
 PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
 
-print("Reading 'full.pkl.bz2' -> data.raw")
-raw = joblib.load("full.pkl.bz2")
 
-print("Reading 'full_scaled.pkl.bz2' -> data.scaled")
-scaled = joblib.load("full_scaled.pkl.bz2")
+def _load(filename):
+    path = PATH / filename
+    print("Reading '{path}')
+    return path
+    
 
-print("Reading 'scaler_full.pkl.bz2' -> data.scaler")
-scaler = joblib.load("scaler_full.pkl.bz2")
+def load_raw():
+  return _load("full.pkl.bz2")
+  
+  
+def load_scaled():
+    return _load("full_scaled.pkl.bz2")
+
+
+def load_scaler():
+    return _load("scaler_full.pkl.bz2")
